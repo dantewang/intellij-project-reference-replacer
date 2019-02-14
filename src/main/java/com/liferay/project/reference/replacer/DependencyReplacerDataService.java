c82a72578006f1731eccfb4cde6eed19a4fac914
Dante Wang <dante.wang@liferay.com>
2019-02-15 00:31:56 +0800

    If a library dependency can be mapped to a module in this project, add the mapped module to the library dependency's owner module as a module dependency

--- src/main/java/com/liferay/project/reference/replacer/DependencyReplacerDataService.java
+++ src/main/java/com/liferay/project/reference/replacer/DependencyReplacerDataService.java
@@ -1,17 +1,20 @@
 package com.liferay.project.reference.replacer;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.externalSystem.model.DataNode;
 import com.intellij.openapi.externalSystem.model.Key;
 import com.intellij.openapi.externalSystem.model.ProjectKeys;
 import com.intellij.openapi.externalSystem.model.project.LibraryDependencyData;
+import com.intellij.openapi.externalSystem.model.project.ModuleData;
 import com.intellij.openapi.externalSystem.model.project.ProjectData;
-import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
+import com.intellij.openapi.externalSystem.service.project.IdeModelsProvider;
 import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService;
 import com.intellij.openapi.externalSystem.util.ExternalSystemConstants;
 import com.intellij.openapi.externalSystem.util.Order;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
-
+import com.intellij.openapi.roots.ModuleRootModificationUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -32,10 +35,12 @@ public class DependencyReplacerDataService extends AbstractProjectDataService<Li
     }
 
     @Override
-    public void importData(
+    public void onSuccessImport(
         @NotNull Collection<DataNode<LibraryDependencyData>> libraryDependencyDataNodes,
         @Nullable ProjectData projectData, @NotNull Project project,
-        @NotNull IdeModifiableModelsProvider modelsProvider) {
+        @NotNull IdeModelsProvider modelsProvider) {
+
+        ModuleManager moduleManager = ModuleManager.getInstance(project);
 
         if(projectData == null) {
             return;
@@ -44,21 +49,41 @@ public class DependencyReplacerDataService extends AbstractProjectDataService<Li
         for (DataNode<LibraryDependencyData> libraryDependencyDataNode : libraryDependencyDataNodes) {
             LibraryDependencyData libraryDependencyData = libraryDependencyDataNode.getData();
 
-            String externalName = libraryDependencyData.getExternalName();
-
-            System.out.println(externalName);
+            String dependencyExternalName = libraryDependencyData.getExternalName();
 
             for (Map.Entry<String, String> artifactMapping : _artifactMappings.entrySet()) {
-                if (!externalName.startsWith(artifactMapping.getKey())) {
+                if (!dependencyExternalName.startsWith(artifactMapping.getKey())) {
                     continue;
                 }
 
-                _doReplaceDependencyWithModuleReference();
+                _doReplaceDependencyWithModuleReference(
+                    moduleManager, dependencyExternalName, libraryDependencyData.getOwnerModule(),
+                    artifactMapping.getValue());
             }
         }
     }
 
-    private void _doReplaceDependencyWithModuleReference() {
+    private void _doReplaceDependencyWithModuleReference(
+        @NotNull ModuleManager moduleManager, @NotNull String dependencyExternalName,
+        @NotNull ModuleData ownerModuleData, @NotNull String targetModuleName) {
+
+        Module targetModule = moduleManager.findModuleByName(targetModuleName);
+
+        if (targetModule == null) {
+            _log.debug(
+                "Target Module {0} for dependency {1} is not found from this project.", targetModuleName,
+                dependencyExternalName);
+
+            return;
+        }
+
+        Module ownerModule = moduleManager.findModuleByName(ownerModuleData.getInternalName());
+
+        if (targetModule == null) {
+            return;
+        }
+
+        ModuleRootModificationUtil.addDependency(ownerModule, targetModule);
     }
 
     private static final String _GROUP_ID = "com.liferay.portal";
@@ -69,5 +94,7 @@ public class DependencyReplacerDataService extends AbstractProjectDataService<Li
             put(_GROUP_ID + ":com.liferay.portal.kernel", "portal-kernel");
         }
     };
+
+    private static final Logger _log = Logger.getInstance(DependencyReplacerDataService.class);
 
 }
