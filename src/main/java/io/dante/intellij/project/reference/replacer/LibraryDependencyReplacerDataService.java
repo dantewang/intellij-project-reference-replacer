5b1a168f86cafbc62cc8821e952ff7cdf46d2e4e
Dante Wang <coolcfan@gmail.com>
2021-12-11 22:58:23 +0800

    Explore the possibility to replace the dependencies by implementing AbstractDependencyDataService.importData

---
+++ src/main/java/io/dante/intellij/project/reference/replacer/LibraryDependencyReplacerDataService.java
@@ -0,0 +1,84 @@
+package io.dante.intellij.project.reference.replacer;
+
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.externalSystem.model.DataNode;
+import com.intellij.openapi.externalSystem.model.Key;
+import com.intellij.openapi.externalSystem.model.ProjectKeys;
+import com.intellij.openapi.externalSystem.model.project.LibraryDependencyData;
+import com.intellij.openapi.externalSystem.model.project.OrderAware;
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
+import com.intellij.openapi.externalSystem.service.project.manage.AbstractDependencyDataService;
+import com.intellij.openapi.externalSystem.util.ExternalSystemConstants;
+import com.intellij.openapi.externalSystem.util.Order;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.LibraryOrderEntry;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.OrderEntry;
+
+import com.intellij.openapi.roots.libraries.Library;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Order(ExternalSystemConstants.UNORDERED)
+public class LibraryDependencyReplacerDataService extends AbstractDependencyDataService<LibraryDependencyData, LibraryOrderEntry> {
+
+	@Override
+	public @NotNull Key<LibraryDependencyData> getTargetDataKey() {
+		return ProjectKeys.LIBRARY_DEPENDENCY;
+	}
+
+	@Override
+	protected @NotNull Class<LibraryOrderEntry> getOrderEntryType() {
+		return LibraryOrderEntry.class;
+	}
+
+	@Override
+	protected Map<OrderEntry, OrderAware> importData(
+		@NotNull Collection<? extends DataNode<LibraryDependencyData>> nodesToImport, @NotNull Module module,
+		@NotNull IdeModifiableModelsProvider modelsProvider) {
+
+		Map<OrderEntry, OrderAware> orderEntryDataMap = new LinkedHashMap<>();
+
+		for (DataNode<LibraryDependencyData> nodeToImport : nodesToImport) {
+			LibraryDependencyData libraryDependencyData = nodeToImport.getData();
+
+			String moduleName = ReplacementUtil.findReplacementModuleName(libraryDependencyData);
+
+			if (moduleName == null) {
+				continue;
+			}
+
+			Module replacementDependencyModule = modelsProvider.findIdeModule(moduleName);
+
+			if (replacementDependencyModule == null) {
+				continue;
+			}
+
+			ModifiableRootModel modifiableRootModel = modelsProvider.getModifiableRootModel(module);
+
+			LibraryOrderEntry originalDependency = (LibraryOrderEntry) modelsProvider.findIdeModuleOrderEntry(
+				libraryDependencyData);
+
+			assert originalDependency != null;
+
+			Library library = originalDependency.getLibrary();
+
+			assert library != null;
+
+			modifiableRootModel.removeOrderEntry(
+				Objects.requireNonNull(modifiableRootModel.findLibraryOrderEntry(library)));
+
+			OrderEntry orderEntry = ReadAction.compute(
+				() -> modifiableRootModel.addModuleOrderEntry(replacementDependencyModule));
+
+			orderEntryDataMap.put(orderEntry, libraryDependencyData);
+		}
+
+		return orderEntryDataMap;
+	}
+
+}
